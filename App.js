15b3c2182441f3399cb31d7ad46a55dd18561e1e
Divya9515 <50729779+Divya9515@users.noreply.github.com>
2021-03-12 17:23:18 +0530

    Add files via upload
    There are the files for creating a chat application using react-chat-engine

---
+++ App.js
@@ -0,0 +1,18 @@
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+const App=()=>{
+    if(!localStorage.getItem('username')) return <LoginForm />
+    return(
+        <ChatEngine 
+            height="100vh"
+            projectID="ec735b29-6aac-446d-ba96-57f477ce3424"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+        />
+    );
+}
+
+export default App;
